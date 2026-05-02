ae7766f539e26e6836f6c1bb3ecf4ae24d4face3
dearnasaruddin <dearnasaruddin@gmail.com>
2026-05-02 06:01:12 +0600

    login screen design done

--- app/index.tsx
+++ app/index.tsx
@@ -10,8 +10,8 @@ export default function Index() {
       <Image source={require('../assets/images/corner_shape.webp')} style={{ width: 200, height: 180, position: 'absolute', top: 0, left: 0 }} />
       <View className=" flex-1 justify-end items-center">
         <Image source={require('../assets/images/get_started.webp')} style={{ width: 254, height: 194 }} />
-        <Text className="text-center text-lg font-poppins-bold text-black mt-16 mb-4">Gets things with TODs</Text>
-        <Text className="text-center px-8 font-poppins leading-[157%] text-black/80]">Lorem ipsum dolor sit amet consectetur. Eget sit nec et euismod. Consequat urna quam felis interdum quisque. Malesuada adipiscing tristique ut eget sed.</Text>
+        <Text className="text-center text-lg font-poppins-bold text-black mt-16 mb-4">Gets things with CHECKLIO</Text>
+        <Text className="text-center px-8 font-poppins leading-[157%] text-[rgba(0,0,0,0.7)]">Lorem ipsum dolor sit amet consectetur. Eget sit nec et euismod. Consequat urna quam felis interdum quisque. Malesuada adipiscing tristique ut eget sed.</Text>
         <View className="px-7 w-full mt-[120px] mb-[72px]">
           <TouchableOpacity onPress={() => router.push('/login')} className="py-4 bg-[#50C2C9] rounded-lg" >
             <Text className="font-poppins-semibold text-lg text-white text-center">Get Started</Text>
--- app/login.tsx
+++ app/login.tsx
@@ -1,10 +1,34 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, TouchableOpacity, TextInput } from 'react-native'
+import { Link, router } from 'expo-router'
+import { Image } from 'expo-image'
+import { StatusBar } from 'expo-status-bar'
 
 export default function login() {
   return (
-    <View>
-      <Text>login</Text>
+    <View className="flex-1 relative">
+
+      <StatusBar style="dark" />
+      <Image source={require('../assets/images/corner_shape.webp')} style={{ width: 200, height: 180, position: 'absolute', top: 0, left: 0 }} />
+
+      <View className=" flex-1 justify-end items-center">
+        <Text className="text-center text-lg font-poppins-bold text-black mt-16 mb-[52px]">Welcome Back</Text>
+        <Image source={require('../assets/images/login.webp')} style={{ width: 184, height: 138 }} />
+
+        <View className='mt-[88px] mx-6 items-center gap-[30px]'>
+          <TextInput className='w-[380px] rounded-[100px] py-4 px-6 bg-white' placeholder='Enter your Email' />
+          <TextInput secureTextEntry className='w-[380px] rounded-[100px] py-4 px-6 bg-white' placeholder='Enter your Password' />
+        </View>
+
+        <Text className='font-poppins text-sm leading-[157%] text-[#50C2C9] my-12'>Forgot password?</Text>
+
+        <View className="px-7 w-full mb-5">
+          <TouchableOpacity onPress={() => router.push('/login')} className="py-4 bg-[#50C2C9] rounded-lg" >
+            <Text className="font-poppins-semibold text-lg text-white text-center">Login</Text>
+          </TouchableOpacity>
+        </View>
+
+        <Text className='font-poppins text-base leading-[157%] mb-[30px]'>Don't have an account? <Link href="/signup" className='text-[#50C2C9]'>Sign Up</Link></Text>
+      </View>
     </View>
   )
 }
